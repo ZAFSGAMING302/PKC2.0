160cdad19b2c43c461e87e2f3bcbe441a90763de
ZAFSGAMING302 <zb4716010@gmail.com>
2026-01-02 13:35:22 +0500

    ProjectKorraCustom.java

--- src/main/java/me/zain/projectkorra/ProjectKorraCustom.java
+++ src/main/java/me/zain/projectkorra/ProjectKorraCustom.java
@@ -1,18 +1,11 @@
 package me.zain.projectkorra;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import me.zain.projectkorra.listeners.DamageListener;
-import me.zain.projectkorra.weaknesses.WeaknessManager;
-import me.zain.projectkorra.elements.ElementRegistry;
 
 public class ProjectKorraCustom extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        ElementRegistry.registerAll();
-        WeaknessManager.load();
-        Bukkit.getPluginManager().registerEvents(new DamageListener(), this);
-        getLogger().info("ProjectKorraCustom Enabled!");
+        getLogger().info("Plugin successfully started (no ProjectKorra dependency).");
     }
 }
